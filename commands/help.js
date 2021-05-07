9c88356c111bcec7ca324e3072af50ecc01cd489
Blake Allen <blakesnake100@gmail.com>
2021-05-07 16:39:44 -0700

    improve help command

--- commands/help.js
+++ commands/help.js
@@ -5,11 +5,12 @@ const ping = {
 	execute(message, args) {
 		return message.channel.send(`
       How to count: The decimal-counting channel has split into factions. Each factions counts differently. Choose a faction using !pledge [faction name].
-      \nLoyalists go by the total channel message count (count = total number of previous messages on the channel + 1).
-      \nRebels reject the total message count. Instead, they just increment each other's counts. (count = last count by a member of the rebel faction + 1)
-      \nNihilists ignore both the loyalists and rebels. They restarted the message count from 1 a while ago. They increment each other's counts (count = last count by a "faction with no name" member + 1)
-      \nEccentrists restarted when the nihilists did, but thereafter based the count on the number of messages since the nihilist epoch (count = last count by an eccentric + 1)
-      \nUse !explain for a more comprehensive explanation.
+      \nLoyalists go by the total channel message count. The O.Gs.
+      \nRebels reject the total message count. Instead, they just increment each other's counts, ignoring the other counts. The cool kids.
+      \nNihilists restarted the message count from 1 a while ago because they didn’t care enough to deal with the battle between the loyalists and rebels. Nihilists only increment counts from other nihilists.
+      \nEccentrists also restarted, but they base the count on the number of messages since the restart. The weirdos. They use two search queries and a gd calculator to find the right count.
+      \nCompromisers include the counts of all the other factions. This is currently popular, because it helps keep track of the counts before faction roles are created. The peacekeepers.
+      
     `);
 	}
 }
